d1770243d644bbe01f40529366694d059c27d531
Jmhano <jmhano@gmail.com>
2023-01-15 16:22:25 -0500

    added quesries to server file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -8,18 +8,47 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employees'
-    },
-    console.log('Connected to the employee database.')
-  );
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employees",
+  },
+  console.log("Connected to the employee database.")
+);
 
-  db.query(`SELECT * FROM employees`, (err, rows) => {
-    console.log(rows);
-  });
+// db.query(`SELECT * FROM employees`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// // GET a single employee
+// db.query(`SELECT * FROM employees WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+//   // Delete a employee
+// db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+//   // Create a employee
+// const sql = `INSERT INTO employees (id, first_name, last_name,role_id, manager_id ) 
+// VALUES (?,?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+// if (err) {
+// console.log(err);
+// }
+// console.log(result);
+// });
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
